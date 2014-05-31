de5cc71f106e89fd7167c66ffa7c42fb19e901cb
tysonanderson <tyson@Tysons-MacBook-Pro.local>
2014-05-31 15:51:03 -0700

    update

--- app/app/scripts/app.js
+++ app/app/scripts/app.js
@@ -1,6 +1,29 @@
 /*global define */
-define([], function () {
+define(['d3'], function (d3) {
     'use strict';
 
-    return '\'Allo \'Allo!';
+    //vis variables
+    var width = 960,
+    	height = 100;
+
+    //timeline scale/axis
+    var timeline_x = d3.scale.linear()
+    	.range([0,width])
+    	.domain([1970, 2012])
+    	.nice();
+
+    var timeline = d3.select("#timeline").append("svg");
+
+    var timeline_axis = d3.svg.axis()
+    	.scale(timeline_x)
+    	.orient("bottom")
+    	.ticks(10)
+    	.tickFormat(timeline_x.tickFormat(1,"d"));
+
+    timeline.append("g")
+    	.call(timeline_axis);
+
+
+    console.log( timeline_x(2000) );
+
 });
