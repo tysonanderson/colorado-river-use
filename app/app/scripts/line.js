f4b2786dd244e62ff52335ac2b1ff1f7e431587d
James Estevez <jestevez@pugetsound.edu>
2014-06-01 12:39:42 -0700

    working version

--- app/app/scripts/line.js
+++ app/app/scripts/line.js
@@ -1,123 +1,72 @@
-d3.text('../data/combined.csv', function (unparsedData) {
-    var data = d3.csv.parse(unparsedData),
-        datum = data[0],
-        w = 800,
-        h = 500,
-        margin = 20;
-    
-    console.log("datum", datum);
-     
+var margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-    //var parseDate = d3.time.format('%y').parse;
+var parseDate = d3.time.format("%d-%b-%y").parse;
 
+var x = d3.time.scale()
+    .range([0, width]);
 
+var y = d3.scale.linear()
+    .range([height, 0]);
 
-    var list = d3.range(1971, 2012, 1),
-        vals = [];
-    
-    for (var i = 0; i < list.length; i++) {
-        vals.push({date:  new Date(list[i], 0, 1), value: +datum[ list[i].toString()]})
-    };
-            console.log("vals",vals);
-//    var seriesData = varNames.map(function (name) {
-//      return {
-//        name: name,
-//        values: data.map(function (d) {
-//          return {name: name, label: d[labelVar], value: +d[name]};
-//        })
-//      };
-//    });    console.log("series", seriesData);
-
-    var y = d3.scale.linear()
-        .domain(d3.extent(vals, function(d) { return d.value; }))
-        .range([0 + margin, h - margin]),
-        x = d3.time.scale()
-        .domain(d3.extent(vals, function(d) { return d.date; }))
-        .range([0 + margin, w - margin])
-        
-    var xAxis = d3.svg.axis()
+var xAxis = d3.svg.axis()
     .scale(x)
     .orient("bottom");
 
-    var yAxis = d3.svg.axis()
+var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
-    
-    var svg = d3.select("body") 
-        .append("svg:svg")
-        .attr("width", w)
-        .attr("height", h)
 
-    var g = svg.append("svg:g")
-       .attr("transform", "translate(0, 200)");          
-    
-    var line = d3.svg.line()
-        .x(function(d) { return x(d.date); })
-        .y(function(d) { return y(d.value); }); 
+var line = d3.svg.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.value); });
 
-    svg.append("svg:path").attr("d", line(vals)); 
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv('../data/combined.csv', function (error, data) {
+/*  I structured this by parsing the csv and then reading one user from the array. 
+    It should probably be structured as a function that accepts a record object
+    from the treemap.
+*/    
+    var datum = data[60]; // a test placeholder
+    console.log("datum", datum);
+     
+
+    var list = d3.range(1971, 2012, 1),
+        vals = [];
+    /* Iterate over the range of years and construct an object containing a year-value
+    pair */
+    for (var i = 0; i < list.length; i++) {
+        vals.push({date:  new Date(list[i], 0, 1), value: +datum[ list[i].toString()]})
+    };
     
+    console.log("vals",vals);
+
+    x.domain(d3.extent(vals, function(d) { return d.date; }));
+    y.domain(d3.extent(vals, function(d) { return d.value; }));
+
     svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + h + ")")
-    .call(xAxis);
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Price ($)");
-    
-/*   g.append("svg:line")
-        .attr("x1", x(0))
-        .attr("y1", -1 * y(0))
-        .attr("x2", x(w))
-        .attr("y2", -1 * y(0))
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+      .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Thousand acre feet");
 
-    g.append("svg:line")
-        .attr("x1", x(0))
-        .attr("y1", -1 * y(0))
-        .attr("x2", x(0))
-        .attr("y2", -1 * y(d3.max(data)))
-/*
-    g.selectAll(".xLabel")
-        .data(x.ticks(5))
-        .enter().append("svg:text")
-        .attr("class", "xLabel")
-        .text(String)
-        .attr("x", function(d) { return x(d) })
-        .attr("y", 0)
-        .attr("text-anchor", "middle")
-
-    g.selectAll(".yLabel")
-        .data(y.ticks(4))
-        .enter().append("svg:text")
-        .attr("class", "yLabel")
-        .text(String)
-        .attr("x", 0)
-        .attr("y", function(d) { return -1 * y(d) })
-        .attr("text-anchor", "right")
-        .attr("dy", 4)
-
-    g.selectAll(".xTicks")
-        .data(x.ticks(5))
-        .enter().append("svg:line")
-        .attr("class", "xTicks")
-        .attr("x1", function(d) { return x(d); })
-        .attr("y1", -1 * y(0))
-        .attr("x2", function(d) { return x(d); })
-        .attr("y2", -1 * y(-0.3))
-
-    g.selectAll(".yTicks")
-        .data(y.ticks(4))
-        .enter().append("svg:line")
-        .attr("class", "yTicks")
-        .attr("y1", function(d) { return -1 * y(d); })
-        .attr("x1", x(-0.3))
-        .attr("y2", function(d) { return -1 * y(d); })
-        .attr("x2", x(0))*/
-        })
+    svg.append("path")
+        .datum(vals)
+        .attr("class", "line")
+        .attr("d", line);
+});
