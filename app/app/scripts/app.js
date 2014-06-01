aa4615705d1600884734169cfb46cd0127ceb14f
James Estevez <jestevez@pugetsound.edu>
2014-06-01 10:44:23 -0700

    hark! a line!

--- app/app/scripts/app.js
+++ app/app/scripts/app.js
@@ -1,6 +1,108 @@
 /*global define */
-define([], function () {
+define(['d3'], function (d3) {
     'use strict';
 
-    return '\'Allo \'Allo!';
+    //vis variables
+    var w = 960,
+    	h = 500,
+    	color = d3.scale.category10();
+
+    var color = d3.scale.ordinal( ).range(["#FC432A","#2B2E42","#7BD968"]);
+
+    //timeline scale/axis
+    var timeline_x = d3.scale.linear()
+    	.range([0,w])
+    	.domain([1970, 2012])
+    	.nice();
+
+    var timeline = d3.select("#timeline").append("svg");
+    var tmap = d3.select("#tree_container").append("svg");
+
+    var timeline_axis = d3.svg.axis()
+    	.scale(timeline_x)
+    	.orient("bottom")
+    	.ticks(10)
+    	.tickFormat(timeline_x.tickFormat(1,"d"));
+
+    timeline.append("g")
+    	.call(timeline_axis);
+
+    var treemap = d3.layout.treemap()
+	    .size([w, h])
+	    .sticky(true)
+	    .children(function (d){ return d.values})
+	    .value(function(d) { return d["1971"]; });
+
+
+   	d3.csv("data/combined.csv", function (data){
+
+   		var nest = d3.nest()
+		    .key(function(d) { return d.State; })
+		    .entries(data);
+
+		 //console.log(d3.min(data, function (d){ return +d["2012"]}));
+
+		// var y = d3.scale.linear()
+		// 	.domain([d3.min(data, function (d){ return +d["2012"]}), d3.max(data, function (d){ return +d["2012"]})])
+		// 	.range([0, h]);
+		// console.log(y);
+
+
+		//  tmap.selectAll("rect")
+		//  	.data(data)
+		//  	.enter()
+		//  	.append("rect")
+		//  	.attr("width", 10)
+		//  	.attr("height", function (d){ console.log(+d["2012"]); return y(+d["2012"]) })
+		//  	.attr("x", function (d,i){ return i*11  });
+
+		console.log(nest);
+
+		var ndata = {"key": "colorado_river", "values": nest}
+
+		 // make cells for each 
+		var cell = tmap.data([ndata]).selectAll("g")
+      		.data(treemap.nodes)
+    		.enter().append("g")
+      		.attr("class", "cell")
+      		.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+		cell.append("rect")
+		    .attr("width", function(d) { return d.dx; })
+		    .attr("height", function(d) { return d.dy; })
+		    .style("fill", function(d) { console.log(d.State); return d.values ? color(d.State) : color(d.State); });
+
+		cell.append("text")
+		    .attr("x", function(d) { return d.dx / 2; })
+		    .attr("y", function(d) { return d.dy / 2; })
+		    .attr("dy", ".35em")
+		    .attr("text-anchor", "middle")
+		    .text(function(d) { return (d.depth == 1)? d.key : null ; });
+
+		var initial = 1971;
+
+		var timer = setInterval(function (){
+			if(initial == 2011){
+				clearInterval(timer);
+			}
+
+			$('#year').html("Year: " + initial);
+
+			treemap.value(function(d) { return d[initial]; });
+			initial += 1;
+
+			cell.data(treemap.nodes)
+				.transition()
+	      		.attr("class", "cell")
+	      		.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+			cell.selectAll("rect")
+			    .attr("width", function(d) { return d.dx; })
+			    .attr("height", function(d) { return d.dy; })
+			    .style("fill", function(d) { console.log(d.State); return d.values ? color(d.State) : color(d.State); });
+		}, 1000)
+
+
+   	});
+
 });
--- app/app/scripts/line.js
+++ app/app/scripts/line.js
@@ -1,68 +1,66 @@
-d3.text('../data/combined.csv', function (unparsedData)
-        {
-    var data = d3.csv.parse(unparsedData);
-    var datum = data[0];
-    var w = 400;
-    var h = 200;
-    var margin = 20;
-            console.log("datum", datum);
+d3.text('../data/combined.csv', function (unparsedData) {
+    var data = d3.csv.parse(unparsedData),
+        datum = data[0],
+        w = 800,
+        h = 500,
+        margin = 20;
+    
+    console.log("datum", datum);
+     
 
-    var list = d3.range(1971, 2012, 1);
-            
-    var nest = d3.nest()
-        .key(function(d) { return d.State; })
-        .key(function(d) { return d.Category; })
-        //.key(function(d) { return d['Water User']; })
-        .entries(datum);
-        
-    console.log("nest", nest);
-            
-    var y = d3.scale.linear()
-        .domain([d3.min(data), d3.max(data)])
-        .range([0 + margin, h - margin])
+    //var parseDate = d3.time.format('%y').parse;
 
-    var x = d3.scale.linear()
-        .domain([0, data.length])
-        .range([0 + margin, w - margin])
 
-    console.log(datum['2012']);
-    var vals = []
+
+    var list = d3.range(1971, 2012, 1),
+        vals = [];
+    
     for (var i = 0; i < list.length; i++) {
-        vals.push(  datum[ list[i].toString()])
+        vals.push({date:  new Date(list[i], 0, 1), value: +datum[ list[i].toString()]})
     };
             console.log("vals",vals);
-    var seriesData = varNames.map(function (name) {
-      return {
-        name: name,
-        values: data.map(function (d) {
-          return {name: name, label: d[labelVar], value: +d[name]};
-        })
-      };
-    });
-    console.log("series", seriesData);
+//    var seriesData = varNames.map(function (name) {
+//      return {
+//        name: name,
+//        values: data.map(function (d) {
+//          return {name: name, label: d[labelVar], value: +d[name]};
+//        })
+//      };
+//    });    console.log("series", seriesData);
 
-    var parseDate = d3.time.format('%y').parse;
-
-    var vis = d3.select("body")
+    var y = d3.scale.linear()
+        .domain(d3.extent(vals, function(d) { return d.value; }))
+        .range([0 + margin, h - margin]),
+        x = d3.time.scale()
+        .domain(d3.extent(vals, function(d) { return d.date; }))
+        .range([0 + margin, w - margin])
+        
+       console.log(d3.extent(data, function(d) { return d.date; })) 
+    var svg = d3.select("body") 
         .append("svg:svg")
         .attr("width", w)
         .attr("height", h)
-     
-    var g = vis.append("svg:g")
-        .attr("transform", "translate(0, 200)");
+
+    //var g = svg.append("svg:g")
+     //   .attr("transform", "translate(0, 200)");          
     
     var line = d3.svg.line()
-        .x(function(d,i) { return x(i); })
-        .y(function(d) { return -1 * y(d); }) 
-        
-    g.append("svg:path").attr("d", line(data)); 
+        .x(function(d) { return x(d.date); })
+        .y(function(d) { return y(d.value); }); 
 
-    g.append("svg:line")
+    svg.append("svg:path").attr("d", line(vals)); 
+    
+    svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+ /*   g.append("svg:line")
         .attr("x1", x(0))
         .attr("y1", -1 * y(0))
         .attr("x2", x(w))
         .attr("y2", -1 * y(0))
-     
+
     g.append("svg:line")
         .attr("x1", x(0))
         .attr("y1", -1 * y(0))
@@ -77,7 +75,7 @@ d3.text('../data/combined.csv', function (unparsedData)
         .attr("x", function(d) { return x(d) })
         .attr("y", 0)
         .attr("text-anchor", "middle")
-     
+
     g.selectAll(".yLabel")
         .data(y.ticks(4))
         .enter().append("svg:text")
@@ -96,7 +94,7 @@ d3.text('../data/combined.csv', function (unparsedData)
         .attr("y1", -1 * y(0))
         .attr("x2", function(d) { return x(d); })
         .attr("y2", -1 * y(-0.3))
-     
+
     g.selectAll(".yTicks")
         .data(y.ticks(4))
         .enter().append("svg:line")
@@ -104,5 +102,5 @@ d3.text('../data/combined.csv', function (unparsedData)
         .attr("y1", function(d) { return -1 * y(d); })
         .attr("x1", x(-0.3))
         .attr("y2", function(d) { return -1 * y(d); })
-        .attr("x2", x(0))
+        .attr("x2", x(0))*/
         })
